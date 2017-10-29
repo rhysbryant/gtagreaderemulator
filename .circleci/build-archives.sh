98aa37e9cf3dfac82ec19129e94fab1ce7174ed2
Rhys Bryant <rhys@voltprojects.com>
2017-10-29 13:56:38 +1100

    added circleci config

---
+++ .circleci/build-archives.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mkdir release 
+outputName=""
+cp -R ui tools  release/
+for file in dist/*
+do
+ if [[ "$file" == *"win"* ]]; then
+	new=${file:0:-4}
+	new+=".zip"
+	outputName="tagEmulatorClient.exe"
+ else
+	new=$file
+	new+=".tar.gz"
+	outputName="tagEmulatorClient"
+ fi;
+ cp $configFile release/config.json -f
+ cp $file release/$outputName -f
+ cd release/
+ archiver make "../$new" $outputName
+ cd ..
+done;
+
+mkdir packages
+mv dist/*.zip dist/*.gz packages/
